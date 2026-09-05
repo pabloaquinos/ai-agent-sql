89cf288adad6e4ab86ccc107446835bd04db4d2c
Pablo Jesus Aquino da Silva <pablosilva0818@gmail.com>
2026-09-05 20:11:51 -0300

    feat(python): configura o ambiente virtual, setting e conexao com o banco de dados

---
+++ src/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    db_host: str
+    db_port: int = 5432
+    db_name: str
+    db_user: str
+    db_password: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
